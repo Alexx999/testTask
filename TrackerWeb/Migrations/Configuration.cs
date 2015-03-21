fa4fbcf3a461bf13e490eb24d4e17984e6c20905
Alexander Vostres <alexx999@gmail.com>
2015-03-21 23:54:44 +0200

    Enable CodeFirst migrations

---
+++ TrackerWeb/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace TrackerWeb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TrackerWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "TrackerWeb.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(TrackerWeb.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
